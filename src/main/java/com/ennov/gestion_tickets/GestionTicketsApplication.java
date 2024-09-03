d4d11311e0cf6c9aee33b5c6fab9fe1f4780b51e
mouhamedmane <mouhamed.mane@outlook.com>
2024-09-03 20:47:12 +0000

    Initialisation Commit

---
+++ src/main/java/com/ennov/gestion_tickets/GestionTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.ennov.gestion_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTicketsApplication.class, args);
+	}
+
+}
