d9a51e684d960487cc5222b3d9daeb60515b1729
mouhamedmane <mouhamed.mane@outlook.com>
2024-09-04 00:40:40 +0000

    Ajout de la reponse uniforme de sortie de l'api

---
+++ src/main/java/com/ennov/gestion_tickets/responseApi/ResponseApi.java
@@ -0,0 +1,24 @@
+package com.ennov.gestion_tickets.responseApi;
+
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ResponseApi<T> {
+    private String status;
+    private T data;
+    private List<String>  errorMessage;
+
+    public static <T> ResponseApi<T> success(T data){
+        return new ResponseApi<>("ok", data, null);
+    }
+
+    public static <T> ResponseApi<T> fail(List<String>  errorMessage){
+        return new ResponseApi<>("echec", null, errorMessage);
+    }
+
+}
